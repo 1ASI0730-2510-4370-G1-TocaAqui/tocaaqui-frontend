05abfec593ba225e4acbc01d64ad0e8f030dab3d
Diego Cabrera <dicabrerabuitron@gmail.com>
2025-05-13 01:43:18 -0500

    feat(login): added the styles of the login section.

--- src/login/pages/login-home.component.vue
+++ src/login/pages/login-home.component.vue
@@ -1,48 +1,42 @@
 <script>
 import { ref } from 'vue';
+import { useToast } from 'primevue/usetoast';
 import { LoginEntity, RegisterEntity } from "../model/login.entity.js";
 import LoginService from "../services/login.service.js";
-import {Button as PvButton} from "primevue";
+import { Button as PvButton } from "primevue";
 
 export default {
   name: "login-home",
-  components: {PvButton},
+  components: { PvButton },
   setup() {
     const name = ref('');
     const email = ref('');
     const password = ref('');
     const selectedRole = ref(null);
-    const errorMessage = ref('');
-    const successMessage = ref('');
+    const toast = useToast();
 
     const handleLogin = async () => {
       const loginEntity = new LoginEntity(name.value, email.value, password.value, selectedRole.value);
       try {
         const user = await LoginService.login(loginEntity);
-
-        successMessage.value = `Inicio de sesión exitoso. Bienvenid@, ${user.name}`;
-        errorMessage.value = '';
-        console.log('Login exitoso:', user);
+        toast.add({ severity: 'success', summary: 'Éxito', detail: `Bienvenid@, ${user.name}`, life: 3000 });
       } catch (error) {
-        errorMessage.value = error;
-        successMessage.value = '';
+        toast.add({ severity: 'error', summary: 'Error', detail: error, life: 3000 });
       }
     };
 
     const handleRegister = async () => {
       if (!name.value || !email.value || !password.value || !selectedRole.value) {
-        errorMessage.value = 'Todos los campos son obligatorios';
+        toast.add({ severity: 'error', summary: 'Error', detail: 'Todos los campos son obligatorios', life: 3000 });
         return;
       }
 
       const registerEntity = new RegisterEntity(name.value, email.value, password.value, selectedRole.value);
       try {
         const response = await LoginService.register(registerEntity);
-        successMessage.value = 'Registro exitoso';
-        errorMessage.value = '';
-        console.log('Registro exitoso:', response);
+        toast.add({ severity: 'success', summary: 'Éxito', detail: 'Registro exitoso', life: 3000 });
       } catch (error) {
-        errorMessage.value = error;
+        toast.add({ severity: 'error', summary: 'Error', detail: error, life: 3000 });
       }
     };
 
@@ -51,8 +45,6 @@ export default {
       email,
       password,
       selectedRole,
-      errorMessage,
-      successMessage,
       handleLogin,
       handleRegister
     };
@@ -62,85 +54,142 @@ export default {
 
 <template>
   <div class="form-container">
-    <div class="logo">
-      <img src="../../assets/vue.svg" alt="Toca Aquí" />
-    </div>
-    <h2 class="text-center">Bienvenido</h2>
-
-    <div class="p-field text-center mb-4">
-      <label class="block mb-2">Selecciona tu rol</label>
-      <div class="p-buttonset">
-        <pv-button
-            :outlined="selectedRole !== 'musico'"
-            :severity="selectedRole === 'musico' ? 'primary' : null"
-            label="Músico / Banda"
-            icon="pi pi-user"
-            @click="selectedRole = 'musico'"
-        />
-        <pv-button
-            :outlined="selectedRole !== 'promotor'"
-            :severity="selectedRole === 'promotor' ? 'primary' : null"
-            label="Promotor / Espacio"
-            icon="pi pi-building"
-            @click="selectedRole = 'promotor'"
-        />
+    <pv-toast></pv-toast>
+    <div class="login-box">
+      <div class="logo">
+        <img src="../../assets/logo-oscuro.png" alt="Toca Aquí" />
       </div>
-    </div>
+      <h2 class="text-center">Bienvenido</h2>
 
-    <div class="p-fluid">
-      <div class="p-field mb-3">
-        <label for="name">Nombre</label>
-        <pv-input-text id="name" v-model="name" placeholder="Ingrese su nombre" />
+      <div class="p-field text-center mb-4">
+        <label class="block mb-2 text-left">Selecciona tu rol</label>
+        <div class="role-buttons">
+          <pv-button
+              :outlined="selectedRole !== 'musico'"
+              :severity="selectedRole === 'musico' ? 'primary' : null"
+              label="Músico / Banda"
+              icon="pi pi-headphones"
+              icon-pos="top"
+              @click="selectedRole = 'musico'"
+          />
+          <pv-button
+              :outlined="selectedRole !== 'promotor'"
+              :severity="selectedRole === 'promotor' ? 'primary' : null"
+              label="Promotor / Espacio"
+              icon="pi pi-user"
+              icon-pos="top"
+              @click="selectedRole = 'promotor'"
+          />
+        </div>
       </div>
 
-      <div class="p-field mb-3">
-        <label for="email">Correo electrónico</label>
-        <pv-input-text
-            id="email"
-            v-model="email"
-            placeholder="Ingrese su correo electrónico"
-        />
+      <div class="p-fluid">
+        <div class="p-field mb-3">
+          <label for="name">Nombre</label>
+          <pv-input-text
+              id="name"
+              v-model="name"
+              placeholder="Ingrese su nombre"
+              class="w-full"/>
+        </div>
+
+        <div class="p-field mb-3">
+          <label for="email">Correo electrónico</label>
+          <pv-input-text
+              id="email"
+              v-model="email"
+              placeholder="Ingrese su correo electrónico"
+              class="w-full"
+          />
+        </div>
+
+        <div class="p-field mb-4">
+          <label for="password">Contraseña</label>
+          <pv-password
+              id="password"
+              v-model="password"
+              toggleMask
+              placeholder="Ingrese su contraseña"
+              input-class="w-full"
+          />
+        </div>
+
+        <pv-button label="Registrarse" class="w-full" @click="handleRegister" />
+        <pv-button label="Iniciar sesión" class="w-full mt-2" @click="handleLogin" />
       </div>
-
-      <div class="p-field mb-4">
-        <label for="password">Contraseña</label>
-        <pv-password
-            id="password"
-            v-model="password"
-            toggleMask
-            placeholder="Ingrese su contraseña"
-        />
-      </div>
-
-      <pv-button label="Registrarse" class="w-full" @click="handleRegister" />
-      <pv-button label="Iniciar sesión" class="w-full mt-2" @click="handleLogin" />
-
-      <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
-      <p v-if="successMessage" class="success">{{ successMessage }}</p>
     </div>
   </div>
 </template>
 
 <style scoped>
+
 .form-container {
-  max-width: 400px;
-  margin: 0 auto;
+  display: flex;
+  height: 100vh;
+  max-width: 600px;
+  margin: auto;
   padding: 2rem;
 }
+
+.login-box {
+  background-color: #f5f5f5;
+  border-radius: 8px;
+  padding: 2rem;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  width: 100%;
+  margin: auto;
+  max-height: 700px;
+}
+
+.role-buttons {
+  display: flex;
+  gap: 1rem;
+  width: 100%;
+}
+
+.role-buttons > * {
+  flex: 1;
+  padding: 2rem 0.5rem;
+  font-size: 1rem;
+}
+
+.p-password {
+  width: 100% !important;
+}
+
+.p-field {
+  margin-bottom: 1.5rem;
+}
+
+.p-field label {
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
 .logo {
   display: flex;
   justify-content: center;
   margin-bottom: 1rem;
 }
+
 .text-center {
   text-align: center;
 }
+
 .error {
   color: red;
   margin-top: 1rem;
 }
+
 .success {
   color: green;
   margin-top: 1rem;
+}
+
+.logo img {
+  max-width: 100px;
+  max-height: 100px;
+  width: auto;
+  height: auto;
 }
 </style>
