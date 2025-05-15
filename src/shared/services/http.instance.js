933ae7ebb11d0523e141fee7e9b7d9025155405b
Oscar Antayhua Castillo <u20191e414@upc.edu.pe>
2025-05-15 16:25:21 -0500

    fix(http): fixed

--- src/shared/services/http.instance.js
+++ src/shared/services/http.instance.js
@@ -1,24 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
 const httpInstance = axios.create({
-    baseURL: BASE_URL,
+    baseURL:  import.meta.env.VITE_API_BASE_URL,
     headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
 });
 
-// Función para obtener todos los datos de los endpoints
-const getAllData = async () => {
-    try {
-        const response = await fetch(`${BASE_URL}/data`);
-        const res = await response.json();
-        const data = res[0]; // porque data es un array
-        return data;
-    } catch (err) {
-        console.error('Error:', err);
-        throw err;
-    }
-};
-
-export { getAllData };
 export default httpInstance;
