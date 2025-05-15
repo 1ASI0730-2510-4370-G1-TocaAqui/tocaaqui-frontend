8293ae99bbae70d9b00f5798ac86433a00c23a00
Oscar Antayhua Castillo <u20191e414@upc.edu.pe>
2025-05-15 16:17:49 -0500

    feat(http): added json server multiple endopints fix

--- src/shared/services/http.instance.js
+++ src/shared/services/http.instance.js
@@ -1,8 +1,24 @@
 import axios from 'axios';
 
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 const httpInstance = axios.create({
-    baseURL:  import.meta.env.VITE_API_BASE_URL,
+    baseURL: BASE_URL,
     headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
 });
 
+// Función para obtener todos los datos de los endpoints
+const getAllData = async () => {
+    try {
+        const response = await fetch(`${BASE_URL}/data`);
+        const res = await response.json();
+        const data = res[0]; // porque data es un array
+        return data;
+    } catch (err) {
+        console.error('Error:', err);
+        throw err;
+    }
+};
+
+export { getAllData };
 export default httpInstance;
